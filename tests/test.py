d3582f253e64d01d2bf565354d9273034041a387
Baptiste Lagarde <baptiste.lagarde@centraliens.net>
2014-01-13 19:45:34 +1100

    CHANGE: Bump the number of test cases to 697. 302 fail - there's work to do

--- tests/test.py
+++ tests/test.py
@@ -3,58 +3,58 @@
 import os
 import unittest
 from ddt import ddt, data
-from unicodecsv import UnicodeReader
-from romkan import to_roma, to_kunrei, to_hepburn, to_kana, to_hiragana, to_katakana
+from unicodecsv import DictReader
+from romkan import to_kunrei, to_hepburn, to_hiragana, to_katakana
 from romkan import is_vowel, is_consonant, expand_consonant
 
 
-def open_csv(path):
-    fh = open(path, 'r')
-    ucsv = UnicodeReader(fh, delimiter=str('	'))
-    return ucsv
-
-
+SEP = '/'   # Separates variants in the same CSV field
 HERE = os.path.abspath(os.path.dirname(__file__))
-TO_ROMA = os.path.join(HERE, 'to_roma.csv')
-TO_KUNREI = os.path.join(HERE, 'to_kunrei.csv')
-TO_HEPBURN = os.path.join(HERE, 'to_hepburn.csv')
-TO_KANA = os.path.join(HERE, 'to_kana.csv')
-TO_HIRAGANA = os.path.join(HERE, 'to_hiragana.csv')
-TO_KATAKANA = os.path.join(HERE, 'to_katakana.csv')
+TESTS_CSV = os.path.join(HERE, 'tests.csv')
+
+
+def get_cases_from_csv(input_field, output_field):
+    fh = open(TESTS_CSV, 'r')
+    ucsv = DictReader(fh, delimiter=str('\t'))
+    for row in ucsv:
+        inputs, outputs = row[input_field], row[output_field]
+        for i in inputs.split(SEP):
+            if outputs != '':
+                yield i, outputs.split(SEP)
 
 
 @ddt
 class ConversionTestCase(unittest.TestCase):
 
-    @data(*open_csv(TO_ROMA))
-    def test_to_roma(self, row):
+    @data(*list(get_cases_from_csv('hiragana', 'hepburn revised')))
+    def test_hiragana_to_hepburn(self, row):
         in_text, expected = row
-        self.assertEquals(to_roma(in_text), expected)
+        self.assertIn(to_hepburn(in_text), expected)
 
-    @data(*open_csv(TO_KUNREI))
-    def test_to_kunrei(self, row):
+    @data(*list(get_cases_from_csv('hiragana', 'kunrei-shiki')))
+    def test_hiragana_to_kunrei(self, row):
         in_text, expected = row
-        self.assertEquals(to_kunrei(in_text), expected)
+        self.assertIn(to_kunrei(in_text), expected)
 
-    @data(*open_csv(TO_HEPBURN))
-    def test_to_hepburn(self, row):
+    @data(*list(get_cases_from_csv('hiragana', 'katakana')))
+    def test_hiragana_to_katakana(self, row):
         in_text, expected = row
-        self.assertEquals(to_hepburn(in_text), expected)
+        self.assertIn(to_katakana(in_text), expected)
 
-    @data(*open_csv(TO_KANA))
-    def test_to_kana(self, row):
+    @data(*list(get_cases_from_csv('katakana', 'hiragana')))
+    def test_katakana_to_hiragana(self, row):
         in_text, expected = row
-        self.assertEquals(to_kana(in_text), expected)
+        self.assertIn(to_katakana(in_text), expected)
 
-    @data(*open_csv(TO_HIRAGANA))
-    def test_to_hiragana(self, row):
+    @data(*list(get_cases_from_csv('wapuro', 'hiragana')))
+    def test_wapuro_to_hiragana(self, row):
         in_text, expected = row
-        self.assertEquals(to_hiragana(in_text), expected)
+        self.assertIn(to_hiragana(in_text), expected)
 
-    @data(*open_csv(TO_KATAKANA))
-    def test_to_katakana(self, row):
+    @data(*list(get_cases_from_csv('wapuro', 'katakana')))
+    def test_wapuro_to_katakana(self, row):
         in_text, expected = row
-        self.assertEquals(to_katakana(in_text), expected)
+        self.assertIn(to_katakana(in_text), expected)
 
 
 class RomkanTestCase(unittest.TestCase):
